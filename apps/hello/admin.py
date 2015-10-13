ed924f974ef69504e3e5c8c38587bd618fad457c
krocozabr <krocozabr@gmail.com>
2015-10-13 11:11:37 +0300

    ticket:1 for 42-test-ollavrova9. Added view, templates, model and css.

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from apps.hello.models import Person
+from django.views.generic import TemplateView
 
-# Register your models here.
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['person'] = Person.objects.first()
+        context['user'] = self.request.user
+        return context
+
+
+home = HomeView.as_view()
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from apps.hello.models import Person
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['person'] = Person.objects.first()
+        context['user'] = self.request.user
+        return context
+
+
+home = HomeView.as_view()

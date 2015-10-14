509d4040c139897a3dd97317b92720057c5cdc84
krocozabr <krocozabr@gmail.com>
2015-10-14 13:24:35 +0300

    ticket:1 for 42-test-ollavrova9. Fix admin.py, settings init.py.

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,15 +1,11 @@
+from django.contrib import admin
 from apps.hello.models import Person
-from django.views.generic import TemplateView
 
 
-class HomeView(TemplateView):
-    template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['person'] = Person.objects.first()
-        context['user'] = self.request.user
-        return context
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birthday',
+                    'email', 'skype', 'jabber')
 
 
-home = HomeView.as_view()
+admin.site.register(Person, PersonAdmin)
+

6db5e11391805f2dbbd0147a1cffc3f09937c599
krocozabr <krocozabr@gmail.com>
2015-10-21 18:23:02 +0300

    ticket:8 for 42-test-ollavrova9.  Added template tag and models.

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,7 +1,16 @@
+import datetime
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from easy_thumbnails.fields import ThumbnailerImageField
 
 size = (200, 200)
+
+ACTION = (
+    ('0', 'delete'),
+    ('1', 'edit'),
+    ('2', 'create'),
+)
 
 
 class Person(models.Model):
@@ -28,3 +37,42 @@ class Requests(models.Model):
 
     def __unicode__(self):
         return str(self.id)
+
+
+class Journal(models.Model):
+    model_name = models.CharField(max_length=25)
+    action = models.CharField(choices=ACTION, max_length=1)
+    timestamp = models.DateTimeField()
+    id_item = models.IntegerField()
+
+    def __unicode__(self):
+        return str(self.id)
+
+
+@receiver(post_save, sender=Requests)
+@receiver(post_save, sender=Person)
+def my_callback_save(sender, instance, signal, *args, **kwargs):
+    if 'created' in kwargs:
+        if kwargs['created']:
+            entry = Journal(model_name=sender.__name__,
+                            action=ACTION[2][1],
+                            timestamp=datetime.datetime.now(),
+                            id_item=instance.id)
+            entry.save()
+    else:
+        if kwargs['update_fields']:
+            entry = Journal(model_name=sender.__name__,
+                            action=ACTION[1][1],
+                            timestamp=datetime.datetime.now(),
+                            id_item=instance.id)
+            entry.save()
+
+
+@receiver(post_delete, sender=Person)
+@receiver(post_delete, sender=Requests)
+def my_callback_delete(sender, instance, signal, *args, **kwargs):
+    entry = Journal(model_name=sender.__name__,
+                    action=ACTION[0][1],
+                    timestamp=datetime.datetime.now(),
+                    id_item=instance.id)
+    entry.save()

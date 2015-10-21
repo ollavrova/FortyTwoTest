487646846a38dd32372a00cdd74553268d606706
krocozabr <krocozabr@gmail.com>
2015-10-21 18:41:12 +0300

    ticket:5 for 42-test-ollavrova9.  Added migrations.

--- apps/hello/migrations/0005_auto__add_field_requests_priority.py
+++ apps/hello/migrations/0005_auto__add_field_requests_priority.py
@@ -8,32 +8,18 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-
-        # Adding model 'Journal'
-        db.create_table(u'hello_journal', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('model_name', self.gf('django.db.models.fields.CharField')(max_length=25)),
-            ('action', self.gf('django.db.models.fields.CharField')(max_length=1)),
-            ('timestamp', self.gf('django.db.models.fields.DateTimeField')()),
-            ('id_item', self.gf('django.db.models.fields.IntegerField')()),
-        ))
-        db.send_create_signal(u'hello', ['Journal'])
+        # Adding field 'Requests.priority'
+        db.add_column(u'hello_requests', 'priority',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
 
 
     def backwards(self, orm):
-        # Deleting model 'Journal'
-        db.delete_table(u'hello_journal')
+        # Deleting field 'Requests.priority'
+        db.delete_column(u'hello_requests', 'priority')
 
 
     models = {
-        u'hello.journal': {
-            'Meta': {'object_name': 'Journal'},
-            'action': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'id_item': ('django.db.models.fields.IntegerField', [], {}),
-            'model_name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
-            'timestamp': ('django.db.models.fields.DateTimeField', [], {})
-        },
         u'hello.person': {
             'Meta': {'object_name': 'Person'},
             'bio': ('django.db.models.fields.TextField', [], {}),
@@ -51,9 +37,18 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Requests'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'request_method': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True'}),
+            'priority': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'request_path': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True'}),
             'row': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
             'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'hello.journal': {
+            'Meta': {'object_name': 'Journal'},
+            'action': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id_item': ('django.db.models.fields.IntegerField', [], {}),
+            'model_name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {})
         }
     }
 

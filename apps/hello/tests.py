303b822dd3cf8827e6d2f8b5876085443713113f
krocozabr <krocozabr@gmail.com>
2015-10-27 19:04:33 +0200

    ticket:3 for 42-test-ollavrova9. Smallfix urls and tests.

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -52,30 +52,6 @@ class TestShowPage(TestCase):
         self.assertEqual(self.person.other, "прочая информация")
 
 
-class TestPage(TestCase):
-    fixtures = ['test.json', ]  # loading test fixtures
-
-    def setUp(self):
-        self.person = Person.objects.first()
-
-    def test_check_page(self):
-        """
-        test check show info on main page with test fixtures
-        """
-        response = self.client.get(reverse('home'))
-        self.assertContains(response, "42 Coffee Cups Test Assignment",
-                            status_code=200)
-        self.assertContains(response, "Test", status_code=200)
-        self.assertContains(response, "TestTest", status_code=200)
-        self.assertContains(response,
-                            "born in 1900, study: school 1955-1995 years",
-                            status_code=200)
-        self.assertContains(response, "test@gmail.com", status_code=200)
-        self.assertContains(response, "test@jabber.ru", status_code=200)
-        self.assertContains(response, "test_skype", status_code=200)
-        self.assertContains(response, "test test", status_code=200)
-
-
 class TestEmptyBase(TestCase):
     def setUp(self):
         self.person = Person.objects.first()
--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
+from apps.hello.views import HomeView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'apps.hello.views.home', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^requests/', 'apps.hello.views.req', name='req'),
 )

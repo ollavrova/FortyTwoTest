c1616bd01251942eb00e73c84d3c10896269360c
krocozabr <krocozabr@gmail.com>
2015-11-04 22:36:09 +0200

    ticket:5 for 42-test-ollavrova9. Added test to check saving in db. Changed README.

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
-42-test template
+42-test project
 ===========================
+A Django 1.6+ project
 
-A Django 1.6+ project template
+###Description
 
-Use fortytwo_test_task.settings when deploying with getbarista.com
-
-### Recomendations
 * apps in apps/ folder
-* use per-app templates folders
-* use per-app static folders
-* use migrations
+* per-app templates folders
+* per-app static folders
+* used migrations
 * use settings.local for different environments
 * common templates live in templates/
 * common static lives in assets/
-* management commands should be proxied to single word make commands, e.g make test
+* management commands proxied to single word make commands, e.g make test
 
+###Requirements packages:
+
+* Django==1.6.7
+* 42cc-pystyle
+* south
+* easy_thumbnails
+* selenium
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -141,6 +141,7 @@ class TestMiddleware(TestCase):
 class TestEditForm(TestCase):
     def setUp(self):
         self.auth = {"username": "admin", "password": "admin"}
+        self.person = Person.objects.first()
 
     def test_auth(self):
         """
@@ -164,7 +165,7 @@ class TestEditForm(TestCase):
         upload_file = open(os.path.join(STATICFILES_DIRS[0],
                                         'img', "test.jpg"), "rb")
         data = dict(
-            first_name="Olga",
+            first_name="Olga1",
             last_name="Test",
             birthday="2000-01-01",
             bio="biography",
@@ -192,6 +193,14 @@ class TestEditForm(TestCase):
         self.assertContains(response2, data['email'])
         self.assertContains(response2, data['skype'])
         self.assertContains(response2, data['other'])
+        # check if it saved in db
+        person = Person.objects.first()
+        self.assertEqual(person.first_name, data['first_name'])
+        self.assertEqual(person.last_name, data['last_name'])
+        self.assertEqual(person.bio, data['bio'])
+        self.assertEqual(person.email, data['email'])
+        self.assertEqual(person.skype, data['skype'])
+        self.assertEqual(person.other, data['other'])
 
     def test_show_errors(self):
         """

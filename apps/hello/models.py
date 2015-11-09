81ce8f613ec3996b9ba4a721270465913dba4bf9
krocozabr <krocozabr@gmail.com>
2015-11-09 15:58:55 +0200

    ticket:13 for 42-test-ollavrova10. Moved signals.

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,7 +1,4 @@
-import datetime
 from django.db import models
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
 from easy_thumbnails.fields import ThumbnailerImageField
 
 size = (200, 200)
@@ -45,24 +42,3 @@ class Journal(models.Model):
 
     def __unicode__(self):
         return str(self.id)
-
-
-@receiver(post_save)
-def my_callback_save(sender, instance=None, created=False, **kwargs):
-    if sender.__name__ not in ['Journal', 'Session']:
-        action = ACTION[2][1] if created else ACTION[1][1]
-        entry = Journal(model_name=sender.__name__,
-                        action=action,
-                        timestamp=datetime.datetime.now(),
-                        id_item=instance.id)
-        entry.save()
-
-
-@receiver(post_delete)
-def my_callback_delete(sender, instance, signal, *args, **kwargs):
-    if sender.__name__ not in ['Journal', 'Session']:
-        entry = Journal(model_name=sender.__name__,
-                        action=ACTION[0][1],
-                        timestamp=datetime.datetime.now(),
-                        id_item=instance.id)
-        entry.save()
---
+++ apps/hello/signals.py
@@ -0,0 +1,25 @@
+import datetime
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from apps.hello.models import Journal, ACTION
+
+
+@receiver(post_save)
+def my_callback_save(sender, instance=None, created=False, **kwargs):
+    if sender.__name__ not in ['Journal', 'Session']:
+        action = ACTION[2][1] if created else ACTION[1][1]
+        entry = Journal(model_name=sender.__name__,
+                        action=action,
+                        timestamp=datetime.datetime.now(),
+                        id_item=instance.id)
+        entry.save()
+
+
+@receiver(post_delete)
+def my_callback_delete(sender, instance, signal, *args, **kwargs):
+    if sender.__name__ not in ['Journal', 'Session']:
+        entry = Journal(model_name=sender.__name__,
+                        action=ACTION[0][1],
+                        timestamp=datetime.datetime.now(),
+                        id_item=instance.id)
+        entry.save()
